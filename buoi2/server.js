c5b261c6ca759672d2559f1fdfb9761acf46a55b
= <=>
2023-03-25 18:30:28 +0700

    upload buoi2

--- buoi2/server.js
+++ buoi2/server.js
@@ -2,23 +2,26 @@
 // npm install express --save
 // npm install --save-dev nodemon
 // npm i
+// npm install ejs
 
 const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.render('index')
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/login.html'));
+  res.render('login')
 });
 
 app.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/register.html'));
+  res.render('register')
 });
 
 app.listen(port, () => {
